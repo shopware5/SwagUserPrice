707be9504214b6e44bf8d1a894699ec286675ffd
David Münter <md@shopware.com>
2014-01-24 12:20:42 +0100

    PT-1529 - Fixed wrong encoding

--- Controllers/Backend/UserPrice.php
+++ Controllers/Backend/UserPrice.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Shopware 4.0
- * Copyright � 2012 shopware AG
+ * Copyright (c) 2012 shopware AG
  *
  * According to our dual licensing model, this program can be used either
  * under the terms of the GNU Affero General Public License, version 3,
